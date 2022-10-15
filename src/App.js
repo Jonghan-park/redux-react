05d70d592a3eb2a14e82acebf87947d695ce64e0
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-15 17:08:13 -0600

    Update counter using redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
 
+const INC = "INC";
+const DEC = "DEC";
+
 const reducer = (state = { counter: 0 }, action) => {
-  return state;
+  // Synchronous Function
+  // We should not mutate the original state
+  switch (action.type) {
+    case INC:
+      return { counter: state.counter + 1 };
+    case DEC:
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer);
